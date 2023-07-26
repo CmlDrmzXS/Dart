0dd07bf0c3a8547531f7d9b60eb117f92f301f9c
CmlDrmzXS <102735642+CmlDrmzXS@users.noreply.github.com>
2023-07-26 06:18:42 +0000

    Variables added

---
+++ variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = "Cemal";
+  String surname = "Durmaz";
+  int birthYear = 2002;
+  double pi = 3.14;
+  bool isBoy = true;
+
+  print(name + " " + surname);
+  print("Birth Year: " + birthYear.toString());
+  print("Pi: " + pi.toString());
+  print("isBoy: " + isBoy.toString());
+}
